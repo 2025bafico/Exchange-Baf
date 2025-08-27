cdabca8201c6f4beec1b770d45fc085ef03f1e03
2025bafico <cmc.comex@icloud.com>
2025-08-27 11:19:07 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Hola, Vercel!</h1>
+      <p>Mi primer deploy con Next.js</p>
+    </div>
+  )
+}
